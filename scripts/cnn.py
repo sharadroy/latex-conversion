1df40d05dc4ceb1ee4ca160a50c5db58fcb529da
gaurav5590 <gk59052@gmail.com>
2018-04-12 18:02:26 +0530

    Model saved and loaded

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np 
 from matplotlib import pyplot as plt 
 import os
+import h5py
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -13,6 +14,7 @@ input_data = input_data.reshape(input_data.shape[0],45,45,1)
 print(input_data.shape)
 labels= np.load('../label.npy')
 labels=keras.utils.to_categorical(labels,40)
+print(labels.shape)
 X_train, X_test, y_train, y_test = train_test_split( input_data, labels, test_size=0.2, shuffle= True, random_state=42)
 
 print(X_train.shape, y_train.shape, X_test.shape,y_test.shape)
@@ -28,7 +30,7 @@ def cnn_model():
 	model.add(Dropout(0.2))
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu'))
-	model.add(Dense(50, activation='relu'))
+	model.add(Dense(80, activation='relu'))
 	model.add(Dense(num_classes, activation='softmax'))
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -38,3 +40,10 @@ model=cnn_model()
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 scores = model.evaluate(X_test, y_test, verbose=0)
 print(scores)
+model_json = model.to_json()
+with open("../model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
